69a490878edd9f111712c920f2d39c91e26e605e
Scarlett <scarlett0413@outlook.com>
2025-05-21 00:01:43 -0400

    Create demo_script.py

---
+++ demo_script.py
@@ -0,0 +1,5 @@
+from analyze_code import analyze_code
+
+# analyze_code("path/to/InputFile.java", "path/to/OutputFile.java", mode="comment")
+analyze_code("MemberRepository.java", "MemberRepository_commented.java", mode="comment")
+
